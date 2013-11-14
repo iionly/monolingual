f9ea40dc76618bf71d3eaf4cde69c37c96d49d42
iionly <iionly@gmx.de>
2013-11-14 20:37:10 +0100

    Initial release for Elgg 1.9; see README.txt

---
+++ monolingual/start.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Removing language selection in user settings
+ * and set user language to site default language at login
+ * if this user is not an admin
+ *
+ * (c) iionly 2012-2013
+ */
+
+elgg_register_event_handler('init', 'system', 'monolingual_init');
+
+function monolingual_init() {
+    // React on login event
+    elgg_register_event_handler('login', 'user', 'monolingual_set_user_language');
+}
+
+/**
+ * Set the user language to site language at login time to take into account the currently selected site language
+ */
+function monolingual_set_user_language($event, $object_type, $object) {
+
+    if($event == 'login' && $object_type=='user' && $object instanceof ElggUser) {
+
+        if (!(elgg_is_admin_user($object->guid))) {
+
+            if (elgg_get_config('language')) {
+                $object->language = elgg_get_config('language');
+                $object->save();
+            }
+        }
+    }
+
+    return true;
+}
---
+++ monolingual/views/default/core/settings/account/language.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Provide a way of setting your language prefs
+ *
+ * @package Elgg
+ * @subpackage Core
+ *
+ * Removing language selection in user settings for non-admins by overwriting core view
+ *
+ * (c) iionly 2012-2013
+ *
+ */
+
+$user = elgg_get_page_owner_entity();
+
+if ($user && elgg_is_admin_user($user->guid)) {
+        $title = elgg_echo('user:set:language');
+        $content = elgg_echo('user:language:label') . ': ';
+        $content .= elgg_view("input/dropdown", array(
+                'name' => 'language',
+                'value' => $user->language,
+                'options_values' => get_installed_translations()
+        ));
+        echo elgg_view_module('info', $title, $content);
+} elseif ($user && elgg_get_config('language')) {
+        echo elgg_view('input/hidden', array('name' => 'language', 'value' => elgg_get_config('language')));
+}
